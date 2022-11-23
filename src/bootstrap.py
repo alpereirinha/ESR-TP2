06630d0912008d27fca7b78adcde0f036d8ccf8d
NaNdUSL <fernandoamlobo2000@gmail.com>
2022-11-23 21:20:36 +0000

    funciona

--- src/bootstrap.py
+++ src/bootstrap.py
@@ -1,31 +1,54 @@
-from node import Node
+import socket, threading, sys, os, time, json
+from pprint import pprint
 
-import socket
-import threading
+class Bootstrap:
 
-BOOTSTRAP = "bootstrap.json"
-
-class Bootstrap(Node):
     def __init__(self, host, port):
-        super().__init__(host, port)
-        
 
-   """  def main(self):
-        threading.Thread(target=startServer, args=()).start() """
+        self.bootstrap = json.load(open("bootstrap.json", 'r'))
+        self.host = host
+        self.port = int(port)
 
-    def sendNeighbours(self, ip):
+    def getNeighbours(self, clientAddress, clientSocket):
+
+        for elem in self.bootstrap:
+
+            if clientAddress[0] in elem['node']:
+
+                clientSocket.send(' '.join(elem['vizinhos']).encode('utf-8'))
+
+    def servico(self):
 
         try:
-            data = json.load(open('bootstrap.json', 'r'))
-        except:
-            raise IOError
-        
-        for i in data:
-            if ip in i['node']:
-                msg = ' '.join(i['vizinhos'])
-                self.socket.sendto(msg.encode('utf-8'), (ip, 3000))
 
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.bind((self.host, self.port))
+            s.listen()
+            print(f"tou a ouvir no host {self.host} e no port {self.port}")
+
+        except socket.error as m:
+
+            print(f"não consegui criar, tá tudo lixado: {m}")
+
+        while True:
+
+            clientSocket, clientAddress = s.accept()
+            print(f"aceitei o gajo com ip {clientAddress}")
+            data = clientSocket.recv(1024)
+
+            # ADDME
+
+            if data.decode('utf-8').split(' ')[0] == "JOIN":
+
+                self.getNeighbours(clientAddress, clientSocket)
+
+        s.close()
+
+    def main(self):
+
+        threading.Thread(target=self.servico, args=()).start()
 
 if __name__ == '__main__':
-    server = Server()
-    server.main()
+
+    bootstrap = Bootstrap(sys.argv[1], sys.argv[2])
+    bootstrap.main()
--- src/node.py
+++ src/node.py
@@ -3,106 +3,50 @@ from pprint import pprint
 
 class Node:
 
-    def __init__(self, host):
+    def __init__(self, host, port):
 
-        self.neighbours = set()
-        self.lock = threading.Lock()
         self.host = host
+        self.port = int(port)
+        self.neighbours = set()
 
-    def askNeighbours(self):
-
-        msg = "BOOTSTRAP"
+    def askBootstrap(self):
 
         try:
 
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.bind((self.host, 3000))
-            self.lock.acquire()
-            s.connect(('10.0.0.10', 3000))
-            s.send(msg.encode('utf-8'))
-            resposta = s.recv(1024)
+            # s.bind((self.host, self.port))
+            s.connect(('10.0.0.10', 3000)) # ip server e port
 
-            print(f"Recebi {resposta.decode('utf-8')}")
+            print("espero conectar com 10.0.0.10 no port 3000")
 
-            # modifica os vizinhos
-            lista = list(resposta.decode('utf-8').split(" "))
-            self.neighbours = self.neighbours.union(set(lista))
-            print(self.neighbours)
-            self.lock.release()
+            s.send("JOIN ".encode('utf-8'))
+            res = s.recv(1024)
+
+            print(f"recebi {res.decode('utf-8')}")
+
+            self.neighbours = self.neighbours.union(set(res.decode('utf-8').split(" ")))
+
+            print(f"info que tenho dos vizinhos: {self.neighbours}")
+
             s.close()
 
         except socket.error as m:
 
-            print(m)
+            print(f"não consegui criar, tá tudo lixado: {m}")
 
     def servico(self):
 
-        try:
-
-            self.askNeighbours()
-            self.msgToNeighbours()
-
-        except socket.error as m:
-            print(m)
-
-# tornar esta cena tcp decente
-
-    def msgToNeighbours(self):
-
-        try:
-
-            print(list(self.neighbours))
-            for x in list(self.neighbours):
-
-                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                s.bind((self.host, 3000))
-                s.connect((x, 4000))
-                msg = "ADDME " + str(self.host)
-                s.send(msg.encode('utf-8'))
-                print(f"Enviei para {x}")
-                s.close()
-
-        except socket.error as m:
-            print(m)
-
-    def getNotification(self):
-
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind((self.host, 4000))
-        s.listen()
-        print('tou a ouvir')
-        clientSocket, clientAddress = s.accept()
-        print('aceitei')
-
         while True:
 
-            try:
-
-                data = clientSocket.recv(1024)
-
-                if data != b'':
-
-                    aux_msg = data.decode('utf-8').split(' ')
-
-                    if aux_msg[0] == "ADDME":
-
-                        self.lock.acquire()
-                        self.neighbours.add(aux_msg[1])
-                        print("ADDME: ")
-                        print(self.neighbours)
-                        self.lock.release()
-                    # clientSocket.send(data)
-
-            except socket.error as m:
-
-                print(m)
-                break
+            time.sleep(3)
+            print(self.neighbours)
 
     def main(self):
 
+        self.askBootstrap()
         threading.Thread(target=self.servico, args=()).start()
-        threading.Thread(target=self.getNotification, args=()).start()
 
 if __name__ == '__main__':
-    node = Node(sys.argv[1])
+
+    node = Node(sys.argv[1], sys.argv[2])
     node.main()
